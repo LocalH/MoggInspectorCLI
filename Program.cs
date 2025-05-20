c6ae258158dff4a2141f027eabd1a34bc43c8047
LocalH <localhbci@gmail.com>
2025-05-19 23:22:28 -0400

    removing tui libs as this is going to be straight CLI, no real code yet

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-﻿namespace MoggInspectorCLI
+﻿using MoggInspectorLib;
+
+namespace MoggInspectorCLI
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("MoggInspectorCLI v1.0 by LocalH");
+            Console.WriteLine("skeleton project for the moment");
         }
     }
 }
